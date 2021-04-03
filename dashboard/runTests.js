cea690a509d42fb2a7806a9300b8061a74a1d608
wfouad <fouad@lepotcommun.fr>
2021-04-03 20:31:57 +0200

    Add dashboard test

---
+++ dashboard/runTests.js
@@ -0,0 +1,30 @@
+'use strict';
+
+process.env.BABEL_ENV = 'test';
+process.env.NODE_ENV = 'test';
+process.env.PUBLIC_URL = '';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+// Ensure environment variables are read.
+require('dotenv').config({
+  path: './environments/.development.env',
+});
+
+
+const jest = require('jest');
+let argv = process.argv.slice(2);
+
+if (
+  argv.indexOf('--watchAll') === -1 &&
+  argv.indexOf('--watchAll=false') === -1
+) {
+  argv.push('--watch');
+}
+
+jest.run(argv);
